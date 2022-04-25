238b5fc8670cfa4cf7e83611588de8729539bd52
michael shintani <mikeshintani@yahoo.com>
2022-04-25 09:04:59 -0700

    writing views.py completed + interpreter connected + superuser created + sqlDB and settings matched up

--- songs/views.py
+++ songs/views.py
@@ -13,7 +13,7 @@ from songs import serializers
 @api_view(['GET', 'POST'])
 def music(request):
     songs = Song.objects.all()
-    song_types_param = request.queery_params.get('title')
+    song_types_param = request.query_params.get('title')
     sort_param = request.query_params.get('sort')
     custom_response_dictionary = {}
     if song_types_param:
@@ -24,3 +24,28 @@ def music(request):
         songs = songs.order_by(sort_param)
         serializer = SongSerializer(songs, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    if request.method == 'GET':
+        songs = Song.objects.all()
+        serializer = SongSerializer(songs, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = SongSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def music_detail(request, pk):
+    songs = get_object_or_404(Song, pk=pk)
+    if request.method == 'GET':
+        serializer = SongSerializer(songs)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'PUT':
+        serializer = SongSerializer(songs, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'DELETE':
+        songs.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
